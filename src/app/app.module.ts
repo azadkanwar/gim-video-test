7274bafb39a8edc81c7d792bc3a684419362e603
azad singh <azadsingh@azads-MBP.domain.name>
2020-05-30 19:43:11 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PublisherComponent } from './publisher/publisher.component';
+import { SubscriberComponent } from './subscriber/subscriber.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PublisherComponent, SubscriberComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export default {
+  // Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+  // Do not include the trailing slash. See the README for more information:
+  SAMPLE_SERVER_BASE_URL: 'https://gim-test.herokuapp.com/',
+  // OR, if you have not set up a web server that runs the learning-opentok-php code,
+  // set these values to OpenTok API key, a valid session ID, and a token for the session.
+  // For test purposes, you can obtain these from https://tokbox.com/account.
+  API_KEY: '46767614',
+  SESSION_ID:
+    '2_MX40Njc2NzYxNH5-MTU5MDgzODcyMjc5Mn5vMEJuUmRmL0J5WkdFV1JmZ2dYRFZvM29-fg',
+  TOKEN:
+    'T1==cGFydG5lcl9pZD00Njc2NzYxNCZzaWc9NTAzNjM1N2MzMjU4ZmE2NDEyODRhNjcxOTM3Nzg4ODMwOTNkZDc3ZjpzZXNzaW9uX2lkPTJfTVg0ME5qYzJOell4Tkg1LU1UVTVNRGd6T0RjeU1qYzVNbjV2TUVKdVVtUm1MMEo1V2tkRlYxSm1aMmRZUkZadk0yOS1mZyZjcmVhdGVfdGltZT0xNTkwODM4NzY0Jm5vbmNlPTAuNTc2ODExMzIwOTA3ODkwMiZyb2xlPW1vZGVyYXRvciZleHBpcmVfdGltZT0xNTkwODYwMzY0JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9',
+};
